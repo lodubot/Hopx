d6cf14947717ac86a10eaa263bfcc71d29f90b54
lodubot <jaihacku@gmail.com>
2026-08-27 09:28:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
-
 from hopx_ai import Sandbox
 
 sandbox = Sandbox.create(
     template="code-interpreter",
-    api_key=os.getenv("hopx_live_JOdRbWpHzHS0.1bX_Lckj-Nkl7nTWYDvSaDHtnec4mSEqDTngCylgBn0")
+    api_key="hopx_live_JOdRbWpHzHS0.1bX_Lckj-Nkl7nTWYDvSaDHtnec4mSEqDTngCylgBn0"
 )
 
 print(f"Sandbox ID: {sandbox.sandbox_id}")
