ce8c848b6d647b8812edb68d35b433133560fd8c
lodubot <jaihacku@gmail.com>
2026-08-27 09:23:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from hopx_ai import Sandbox
+
+sandbox = Sandbox.create(
+    template="code-interpreter",
+    api_key=os.getenv("hopx_live_JOdRbWpHzHS0.1bX_Lckj-Nkl7nTWYDvSaDHtnec4mSEqDTngCylgBn0")
+)
+
+print(f"Sandbox ID: {sandbox.sandbox_id}")
+
+# Execute code in the sandbox
+result = sandbox.run_code("print('Hello from HopX!')")
+print(result.stdout)  # "Hello from HopX!"
+
+# Cleanup
+sandbox.kill()
